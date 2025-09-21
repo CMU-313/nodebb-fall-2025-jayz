b18dcc6f9bd0e4fede58c1e2bf9e866916d15fc9
ZhYGuoL <zguoliau@andrew.cmu.edu>
2025-09-21 15:49:06 +0000

    Add admin poll management functionality

---
+++ src/controllers/admin/polls.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const pollsController = module.exports;
+
+pollsController.get = async function (req, res) {
+	// Render the poll creation admin page
+	res.render('admin/manage/polls', {
+		title: 'Poll Management',
+		breadcrumbs: [
+			{ text: 'Admin', url: '/admin' },
+			{ text: 'Manage', url: '/admin/manage' },
+			{ text: 'Polls' }
+		]
+	});
+};
