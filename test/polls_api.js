81099a7f790d003883810d158b733020459f807a
jaiminfee <151575712+jaiminfee@users.noreply.github.com>
2025-10-11 07:12:57 -0400

    Fixed Controller tests using exisiting users

--- test/polls_api.js
+++ test/polls_api.js
@@ -1,124 +1,58 @@
 'use strict';
 
-const winston = require('winston');
-const assert = require('assert');
-
-// Use the same approach as backend tests
-const db = require('./mocks/databasemock');
 const pollsController = require('../src/controllers/polls');
-const Polls = require('../src/polls/redis');
+const assert = require('assert');
+const { adminUID1, user1 } = require('../src/polls/redis');
 
-describe('Polls Controller', function () {
-    this.timeout(30000);
-    let req, res, next;
-    let originalCreatePoll, originalAddOption, originalGetPolls;
-    let originalVote, originalGetResults, originalGetPoll;
-    let capturedCalls;
+function makeRes() {
+	const res = {};
+	res.statusCode = 200;
+	res.body = null;
+	res.status = function (code) { this.statusCode = code; return this; };
+	res.json = function (data) { this.body = data; return this; };
+	return res;
+}
+describe('Polls Controller (reuse users)', function () {
+	it('should create a poll with admin', async function () {
+		const req = { body: { title: 'Controller test poll' }, uid: adminUID1 };
+		const res = makeRes();
+		const next = (err) => { if (err) throw err; };
+		await pollsController.create(req, res, next);
+		assert.strictEqual(res.statusCode, 200);
+		assert.strictEqual(res.body.status.code, 'ok');
+	});
 
-    beforeEach(function () {
-        // Setup request mock
-        req = {
-            body: {},
-            params: {},
-            uid: 1,
-        };
 
-        // Setup response mock
-        const jsonResponse = [];
-        res = {
-            statusCode: 200,
-            status: function (code) {
-                this.statusCode = code;
-                return this;
-            },
-            json: function (data) {
-                jsonResponse.push(data);
-                return this;
-            },
-            _getJson: function () {
-                return jsonResponse[jsonResponse.length - 1];
-            },
-        };
+	it('should add an option to a poll', async function () {
+		const req = { params: { id: '1' }, body: { text: 'Option A', sort: 0 }, uid: adminUID1 };
+		const res = makeRes();
+		const next = (err) => { if (err) throw err; };
+	});
 
-        // Setup next function
-        const nextCalls = [];
-        next = function (err) {
-            nextCalls.push(err);
-        };
-        next._getCalls = function () {
-            return nextCalls;
-        };
+	it('should vote on a poll', async function () {
+		const req = { params: { id: '1' }, body: {}, uid: user1 };
+		const res = makeRes();
+		const next = (err) => { if (err) throw err; };
 
-        // Store original methods and setup capture
-        capturedCalls = {};
+		await pollsController.vote(req, res, next);
 
-        originalCreatePoll = Polls.createPoll;
-        Polls.createPoll = function (...args) {
-            capturedCalls.createPoll = args;
-            return Promise.resolve(1);
-        };
+		assert.strictEqual(res.statusCode, 200);
+		assert.strictEqual(res.body.status.code, 'ok');
+		assert.strictEqual(res.body.response.success, true);
+	});
 
-        originalAddOption = Polls.addOption;
-        Polls.addOption = function (...args) {
-            capturedCalls.addOption = args;
-            return Promise.resolve(5);
-        };
+	it('should get poll results', async function () {
+		const req = { params: { id: '1' } };
+		const res = makeRes();
+		const next = (err) => { if (err) throw err; };
 
-        originalGetPolls = Polls.getPolls;
-        Polls.getPolls = function (...args) {
-            capturedCalls.getPolls = args;
-            return Promise.resolve([{ id: 1, title: 'Poll 1' }]);
-        };
+		await pollsController.results(req, res, next);
 
-        // Add missing mocks to prevent hanging
-        originalVote = Polls.vote;
-        Polls.vote = function (...args) {
-            capturedCalls.vote = args;
-            return Promise.resolve({ success: true });
-        };
+		assert.strictEqual(res.statusCode, 200);
+		assert.strictEqual(res.body.status.code, 'ok');
+		assert.ok(res.body.response.results.totalVotes >= 0);
+	});
 
-        originalGetResults = Polls.getResults;
-        Polls.getResults = function (...args) {
-            capturedCalls.getResults = args;
-            return Promise.resolve({
-                options: [],
-                totalVotes: 10,
-            });
-        };
-
-        originalGetPoll = Polls.getPoll;
-        Polls.getPoll = function (...args) {
-            capturedCalls.getPoll = args;
-            return Promise.resolve({
-                id: '1',
-                title: 'Sample Poll',
-                options: [],
-            });
-        };
-    });
-
-    afterEach(function () {
-        // Restore original methods
-        Polls.createPoll = originalCreatePoll;
-        Polls.addOption = originalAddOption;
-        Polls.getPolls = originalGetPolls;
-        Polls.vote = originalVote;
-        Polls.getResults = originalGetResults;
-        Polls.getPoll = originalGetPoll;
-    });
-
-    // Tests successful poll creation with valid title and settings
-    describe('create', function () {
-        it('should create a poll successfully', async function () {
-            req.body = { title: 'Test Poll', settings: { multi: false } };
-
-            await pollsController.create(req, res, next);
-
-            const response = res._getJson();
-            console.log('Response:', response);
-            console.log('Captured calls:', capturedCalls.createPoll);
-            assert.strictEqual(response.status.code, 'ok');
-            assert.deepStrictEqual(capturedCalls.createPoll, ['Test Poll', 1, { multi: false }]);
-        });
-    });
 });
+
+
