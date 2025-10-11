ef42a36e7f1d5752849d72193a536332cded2b9c
jaiminfee <151575712+jaiminfee@users.noreply.github.com>
2025-10-11 05:43:30 -0400

    Update logs.js

--- src/meta/logs.js
+++ src/meta/logs.js
@@ -8,23 +8,9 @@ const Logs = module.exports;
 Logs.path = path.resolve(__dirname, '../../logs/output.log');
 
 Logs.get = async function () {
-	try {
-		return await fs.promises.readFile(Logs.path, 'utf-8');
-	} catch (err) {
-		if (err.code === 'ENOENT') return ''; // return empty log if missing
-		throw err;
-	}
+	return await fs.promises.readFile(Logs.path, 'utf-8');
 };
 
 Logs.clear = async function () {
-	try {
-		await fs.promises.truncate(Logs.path, 0);
-	} catch (err) {
-		if (err.code === 'ENOENT') {
-			await fs.promises.mkdir(path.dirname(Logs.path), { recursive: true });
-			await fs.promises.writeFile(Logs.path, '');
-		} else {
-			throw err;
-		}
-	}
+	await fs.promises.truncate(Logs.path, 0);
 };
