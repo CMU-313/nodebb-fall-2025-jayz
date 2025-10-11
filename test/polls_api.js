f91cce4382517e6881c47d20031e28e911ad0980
jaiminfee <151575712+jaiminfee@users.noreply.github.com>
2025-10-11 04:52:07 -0400

    Update polls_api.js

--- test/polls_api.js
+++ test/polls_api.js
@@ -1,27 +1,9 @@
 'use strict';
-console.log('HERE1');
-
-const assert = require('assert');
-const Polls = require('../src/polls/redis');
-console.log('HERE2');
-
-// ----------------------------
-// MOCK POLLS METHODS
-// ----------------------------
-Polls.createPoll = async (title, uid, settings) => 'fakePollId';
-Polls.addOption = async (uid, pollId, text, sort) => 'fakeOptionId';
-Polls.getPolls = async () => [
-	{ id: '1', title: 'Favorite food' },
-	{ id: '2', title: 'Best programming language' },
-];
-console.log('HERE3');
 
 const pollsController = require('../src/controllers/polls');
-console.log('HERE4');
+const assert = require('assert');
+const { adminUID1, user1 } = require('./redis_polls_integration');
 
-// ----------------------------
-// HELPER RES OBJECT
-// ----------------------------
 function makeRes() {
 	const res = {};
 	res.statusCode = 200;
@@ -30,44 +12,16 @@ function makeRes() {
 	res.json = function (data) { this.body = data; return this; };
 	return res;
 }
-console.log('HERE5');
 
-// ----------------------------
-// TESTS
-// ----------------------------
-describe('Polls Controller (unit tests only)', () => {
-	console.log('HERE6');
-	it('should create a poll successfully', async () => {
-		console.log('HERE45');
-		const req = { body: { title: 'Favorite color?' }, uid: 1 };
-		console.log('HERE46');
+describe('Polls Controller (reuse users)', function () {
+	it('should create a poll with admin', async function () {
+		const req = { body: { title: 'Controller test poll' }, uid: adminUID1 };
 		const res = makeRes();
-		console.log('HERE47');
-		const next = (err) => {
-			if (err) throw err; };
-		
-		console.log('HERE7');
+		const next = (err) => { if (err) throw err; };
 
 		await pollsController.create(req, res, next);
 
 		assert.strictEqual(res.statusCode, 200);
 		assert.strictEqual(res.body.status.code, 'ok');
-		assert.strictEqual(res.body.response.pollId, 'fakePollId');
-		console.log('passed this and still timed out');
 	});
-
-	it('should add an option successfully', async () => {
-		const req = { params: { id: '123' }, body: { text: 'Blue' }, uid: 1 };
-		const res = makeRes();
-		const next = (err) => {
-			if (err) throw err; };
-
-		await pollsController.addOption(req, res, next);
-
-		assert.strictEqual(res.statusCode, 200);
-		assert.strictEqual(res.body.status.code, 'ok');
-		assert.strictEqual(res.body.response.optionId, 'fakeOptionId');
-	});
-
-
 });
